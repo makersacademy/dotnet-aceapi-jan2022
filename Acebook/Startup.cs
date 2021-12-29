f68ea801f4d774efe41292b4a51c7d78a3e6dc63
Kay Lack <kay@makers.tech>
2021-12-29 19:55:07 +0000

    Run migrations on project start

--- Acebook/Startup.cs
+++ Acebook/Startup.cs
@@ -93,8 +93,10 @@ namespace Acebook
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext dbContext)
         {
+            dbContext.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
